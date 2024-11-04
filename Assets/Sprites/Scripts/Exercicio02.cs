78961b10e53457f7624d0e6ffb5dd352ee4584ee
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-04 17:09:28 -0300

    exercicio 02 adicionado

---
+++ Assets/Sprites/Scripts/Exercicio02.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Exercicio02 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] bool powerUp;//true
+    void Start()
+    {
+        if (powerUp == true)
+        {
+            print("Power-Up adquirido");
+        }
+        else
+        {
+            print("Nenhum Power-Up foi localizado");
+        }
+
+        //condição ? Caso vdd : Caso falso 
+
+        print(powerUp ? "Power-up adquirido" : "Power-Up algum foi encontrado");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
