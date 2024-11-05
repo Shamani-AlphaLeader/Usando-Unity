29139230ed6e930505cbd193142a7edcb0398b2c
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-05 17:19:20 -0300

    exercicio07 concluido

--- Assets/Sprites/Scripts/Exercicio07.cs
+++ Assets/Sprites/Scripts/Exercicio07.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class Exercicio07 : MonoBehaviour
 {
+    [SerializeField] int faces;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (faces == 0)
+        {
+            print("O numero de faces do dado nao pode ser:" + faces);
+        }
+        else
+        { 
+        print(Random.Range(1, faces));
+        }
         
+
     }
 
     // Update is called once per frame
