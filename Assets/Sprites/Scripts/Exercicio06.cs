8f878908c14b78da94e5e73d2b8052ed6f56895c
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-05 17:09:39 -0300

    ex07 em andamento

--- Assets/Sprites/Scripts/Exercicio06.cs
+++ Assets/Sprites/Scripts/Exercicio06.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 
 public class Exercicio06 : MonoBehaviour
 {
+    [SerializeField] bool Mago;
+    [SerializeField] bool Guerreiro;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (Mago == true && Guerreiro == false)
+        {
+            print("O mago foi escolhido.");
+        }
+        else if (Mago == false && Guerreiro == true)
+        {
+            print("O Guerreiro foi escolhido");
+        }
+        else if (Mago == true && Guerreiro == true)
+        { 
+            print("apenas um, bro.");
+        }
+            else 
+        {
+            print("so lamento entao, parceiro.");
     }
+        }
 
     // Update is called once per frame
     void Update()
