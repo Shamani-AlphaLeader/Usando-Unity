78c94e62a8d29ed2d4b1ffd4c7a8d4647e217400
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-07 15:36:22 -0300

    Exercicios concluidos

--- Assets/Sprites/Scripts/Exercicio10.cs
+++ Assets/Sprites/Scripts/Exercicio10.cs
@@ -2,10 +2,30 @@ using UnityEngine;
 
 public class Exercicio10 : MonoBehaviour
 {
+    [SerializeField] string tipoItem;
+    [SerializeField] int moeda = 10;
+    [SerializeField] bool escudo = false;
+    [SerializeField] bool pocaoVida = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        switch (tipoItem) 
+        {
+            case "Moeda":
+                moeda++;
+                print("Moeda Coletada" + moeda);
+                break;
+
+            case "Escudo":
+                escudo = true;
+                print("escudo ativado");
+                break;
+
+            case "Pocao":
+                pocaoVida= true;
+                print("Pocao de vida coletada");
+                break;
+        }
     }
 
     // Update is called once per frame
