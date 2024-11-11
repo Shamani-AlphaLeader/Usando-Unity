c18ec9d5edafa8befc2f4c6a68c3fc61009b979b
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-11 16:51:27 -0300

    atividade01 feita

--- Assets/Scenes/Sprites02/Scripts02/atividade01.cs
+++ Assets/Scenes/Sprites02/Scripts02/atividade01.cs
@@ -2,10 +2,23 @@ using UnityEngine;
 
 public class atividade01 : MonoBehaviour
 {
+    [SerializeField] private int regressivaInicio = 5;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(CountdownRoutine());
+    }
+
+    // Coroutine para realizar a contagem regressiva
+    private System.Collections.IEnumerator CountdownRoutine()
+    {
+        for (int i = regressivaInicio; i >= 1; i--)
+        {
+            Debug.Log(i); // Exibe o número no Console da Unity
+            yield return new WaitForSeconds(1); // Aguarda 1 segundo
+        }
+
+        Debug.Log("Início da partida!");
     }
 
     // Update is called once per frame
