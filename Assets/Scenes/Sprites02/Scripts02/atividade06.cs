d8a93191795f5c6cdbfb8b774e9ccfe2eb7b0f73
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-12 16:51:38 -0300

    atividade06 feita

--- Assets/Scenes/Sprites02/Scripts02/atividade06.cs
+++ Assets/Scenes/Sprites02/Scripts02/atividade06.cs
@@ -5,12 +5,31 @@ public class atividade06 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        for (int i = 0; i < 10000; i++)
+        {
+            if (i % 3 != 0 && i % 5 != 0) 
+            {
+                print("Fizz Buzz");
+            }
+            else if (i % 5 == 0)
+            {
+                print("Buzz");
+            }
+            else if (i % 3 == 0)
+            {
+                print("Fizz");
+            }
+            else 
+            { 
+            print(i);
+            }
+        }
         
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
