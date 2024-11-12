8a2ce92a1c1b74c3484b0d4c1e6f7d4a911cb298
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-12 17:24:30 -0300

    atividade08 feita

--- Assets/Scenes/Sprites02/Scripts02/atividade08.cs
+++ Assets/Scenes/Sprites02/Scripts02/atividade08.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class atividade08 : MonoBehaviour
 {
+    string palavra = "Ao Vento";
+    string invertida = "";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = palavra.Length -1; i >= 0; i--) 
+        {
+        invertida += palavra[i];
+        }
+        print("String foi Invertida" +invertida);
     }
 
     // Update is called once per frame
