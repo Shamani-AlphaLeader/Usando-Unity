6aec3d1c43e69278bbc10b42464edfe760974c40
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-12 15:26:20 -0300

    atividade02 feita

--- Assets/Scenes/Sprites02/Scripts02/atividade02.cs
+++ Assets/Scenes/Sprites02/Scripts02/atividade02.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class atividade02 : MonoBehaviour
 {
+    [SerializeField] int dano = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+        for (int i = 0; i <= 5; i++)
+        {
+            print("ataque" + i + ": dano" + dano);
+            dano += 2;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
