5696fceba2bcccc6acc427e04f9e86dc1fb94358
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-12 16:33:33 -0300

    atividade05 feita

--- Assets/Scenes/Sprites02/Scripts02/atividade05.cs
+++ Assets/Scenes/Sprites02/Scripts02/atividade05.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class atividade05 : MonoBehaviour
 {
+    [SerializeField] int dificuldade = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int fase = 1; fase <=6; fase++) 
+        {
+        dificuldade +=5;
+            print("fase" + fase + "dificuldade" + dificuldade);
+        }
     }
 
     // Update is called once per frame
