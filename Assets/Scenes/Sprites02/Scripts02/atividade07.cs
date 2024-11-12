6f39883b3827512c6748fad95fd5348ec4d441b5
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-12 17:10:17 -0300

    atividade07 feita

--- Assets/Scenes/Sprites02/Scripts02/atividade07.cs
+++ Assets/Scenes/Sprites02/Scripts02/atividade07.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class atividade07 : MonoBehaviour
 {
+        int[] numeros = { 3, 5, 7, 10, 68, 23, 71, 18, 97, 1};
+    int maior = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        foreach (var item in numeros) 
+        {
+        if (item > maior)
+            {
+                maior = item;
+            }
+        }
+        print("O maior numero:" + maior);
     }
 
     // Update is called once per frame
