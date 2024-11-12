30108f573717dfe33adb7790385c0f19d8225146
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-12 15:43:30 -0300

    aticidade04 feita

--- Assets/Scenes/Sprites02/Scripts02/atividade04.cs
+++ Assets/Scenes/Sprites02/Scripts02/atividade04.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class atividade04 : MonoBehaviour
 {
+    [SerializeField] int score = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for(int combo = 1; combo <= 7; combo++) 
+        {
+            score += 10;
+        }
+        print("Score Atual:" + score);
     }
 
     // Update is called once per frame
