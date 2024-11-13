dbffede9b8e11e7a7443f98604a2c4cfd25d016b
Shamani-AlphaLeader <brunoshamani7000@gmail.com>
2024-11-13 15:25:43 -0300

    desafio02 concluido

--- Assets/Scenes/Sprites02/Scripts02/Desafio02.cs
+++ Assets/Scenes/Sprites02/Scripts02/Desafio02.cs
@@ -1,11 +1,37 @@
+using System;
 using UnityEngine;
 
 public class Desafio02 : MonoBehaviour
 {
+    [SerializeField] int diaNascimento;
+    [SerializeField] int mesNascimento;
+    [SerializeField] int anoNascimento;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int diaAtual = DateTime.Now.Day;
+        int mesAtual = DateTime.Now.Month;
+        int anoAtual = DateTime.Now.Year;
+
+        int anosVividos = anoAtual - anoNascimento;
+        int mesesVividos = mesAtual - anoNascimento;
+        if(mesesVividos <0) 
+        {
+            anosVividos--;
+            mesesVividos += 12;
+        }
+        int diasVividos = diaAtual - diaNascimento;
+        if (diasVividos <0) 
+        {
+            mesesVividos--;
+            diasVividos += 30;
+            if (mesesVividos < 0) 
+            {
+                anosVividos--;
+                mesesVividos += 12;
+            }
+        }
+        print("Voce viveu: " + diasVividos + "dias," + mesesVividos + "meses," + anosVividos + "anos.");
     }
 
     // Update is called once per frame
